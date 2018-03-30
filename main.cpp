adcd8f4f5becef6e3fdb9e022e603fcf83163cac
lixinrui706 <xli21@mail.csuchico.edu>
2018-03-29 19:15:15 -0700

    final

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author:Xinrui Li
 #include <iostream>
 #include <fstream>
 
@@ -12,7 +12,16 @@ int main()
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
-
+  while(!in.eof())
+  {
+    in>>current;
+    if((current >= 'a' && current <= 'z') ||(current >= 'A' && current <= 'Z'))
+    {
+     if(first == ' ')
+        first = current;
+     last = current;
+    }
+   } 
 
 
   //Then, finally, this displays the censored version:
